af024c19583ee6dd2e37c7e37c6a38489f0f5d39
vijaymhr <vijaymaharjan911@gmail.com>
2019-11-27 12:20:14 +1300

    backend started

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Venturecraft\\Revisionable' => array($vendorDir . '/venturecraft/revisionable/src'),
     'PhpOption\\' => array($vendorDir . '/phpoption/phpoption/src'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
